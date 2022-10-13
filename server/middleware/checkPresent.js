17bdfda7640a5091ff29a3d8898b46b39a05bd7f
arunrajbhar123 <arunrajbhar1998@gmail.com>
2022-10-13 10:08:22 +0530

    change

--- server/middleware/checkPresent.js
+++ server/middleware/checkPresent.js
@@ -3,7 +3,7 @@ const UserModel = require("../model/user.model");
 const CheckUserPresent = async (req, res, next) => {
   const { username } = req.params;
 
-  const check = await UserModel.findOne({ username });
+  const check = await UserModel.findOne({ username }, { __v: 0, _id: 0 });
 
   if (check !== null) {
     return res.status(200).send(check);
